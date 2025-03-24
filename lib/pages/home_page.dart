b51e190e9bda024e043eea298e3dca4e39cf9d80
Naman Mahajan <mahajannaman2020@gmail.com>
2025-03-24 10:16:07 +0530

    Bottom nav bar

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:coffee_sipsync/components/bottom_nav_bar.dart';
+import 'package:coffee_sipsync/const.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+    );
+  }
+}
